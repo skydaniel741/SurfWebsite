9bdba0f0b180f0e087b187164d4d69857f716eb0
daniel <mejre8@gmail.com>
2024-05-06 12:09:27 +1200

    created website

---
+++ routes.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return render_template("home.html")
+
+
+if __name__ == "__main__":
+  app.run(debug=True)

1423bc3d41ac5afb70c721aa440a3d55d4d1c6a9
daniel <mejre8@gmail.com>
2024-05-06 20:33:15 +1200

    making the pages

--- routes.py
+++ routes.py
@@ -6,7 +6,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template("home.html")
+  return render_template("layout.html")
+
+@app.route('/signup')
+def signup():
+  return render_template("signup.html")
+
+@app.route('/login')
+def login():
+  return render_template("login.html")
+
+
+@app.route('/surboards')
+def surfboard():
+  return render_template("surfboards.html")
+
+@app.route('/brands')
+def brands():
+  return render_template("brands.html")
+
+
+@app.route('/checkout')
+def checkout():
+  return render_template("checkout.html")
+
+
+
+
+
 
 
 if __name__ == "__main__":

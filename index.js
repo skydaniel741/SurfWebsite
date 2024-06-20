f95492e8177f928dc01d731310f3978e650d148a
daniel <mejre8@gmail.com>
2024-06-20 14:51:56 +1200

    adding a clock

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var loader = document.getElementById("preloader");
 window.addEventListener("load",function(){
 loader.style.display = "none"})
+
+
+setInterval(setTime, 1000);
+setTime();
+function setTime(){
+    var dt = newDate();
+    time = dt.loLocalTimeString([],{hour:'2-digit',minute:'2-digit'});
+    document.getElementById('date-time').innerHTML;
+}
